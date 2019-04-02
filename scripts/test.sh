872733b993fa85d182090198d8a92fea53f09c9e
root <root@kritsana.anres.ku.ac.th>
2019-04-02 21:56:31 +0700

    ship log with location

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-elastalert-test-rule --config /home/lab/senior-project/config.yaml --start 2016-12-31T20:55:02 --end 2017-01-02T23:58:07 $1
+elastalert-test-rule --config ../config.yaml --start 2016-12-31T20:55:02 --end 2017-01-02T23:58:07 $1
