f5057691b4fa8fcb97d895758da338f7877fcdc6
Kritsana <kritsana.chai@ku.th>
2019-04-03 04:35:57 +0700

    unefficient flatline

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,8 @@
-python -m elastalert.elastalert --config /home/lab/senior-project/config.yaml --start 2019-02-09T18:05:43 --end 2019-02-18T06:36:40 --rule $1 | tr -d "[" |tr -d "]" |python -m json.tool
+#!/bin/bash
+
+basedir="${0/*}"
+if [ $# -eq 1 ] then
+  elastalert --verbose --config $basedir/../config.yaml --rule $1 --start 2019-02-11T00:00:00 --end 2019-02-18T00:00:00
+else
+  echo "Usage: run.sh <rule>"
+fi
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-elastalert-test-rule --config ../config.yaml --start 2019-02-09T18:05:43 --end 2019-02-18T06:36:40 $1
+basedir="${0%/*}"
+if [ "$#" -eq 1 ]; then
+  elastalert-test-rule --config $basedir/../config.yaml --start 2019-02-11T00:00:00 --end 2019-02-18T00:00:00 $1
+else
+  echo "Usage: test.sh <rule>"
+fi
