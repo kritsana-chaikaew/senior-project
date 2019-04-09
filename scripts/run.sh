a9bae06790f3e89016efc85af4698e37b409a608
Kritsana <kritsana.chai@ku.th>
2019-04-10 02:34:15 +0700

    flatline, fix script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-basedir="${0/*}"
-if [ $# -eq 1 ] then
-  elastalert --verbose --config $basedir/../config.yaml --rule $1 --start 2019-02-11T00:00:00 --end 2019-02-18T00:00:00
+basedir="${0%/*}"
+if [ "$#" -eq 1 ]; then
+  elastalert --config $basedir/../config.yaml --rule $1 --start 2019-02-11T00:00:00 --end 2019-02-18T00:00:00
 else
   echo "Usage: run.sh <rule>"
 fi
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@
 
 basedir="${0%/*}"
 if [ "$#" -eq 1 ]; then
-  elastalert-test-rule --config $basedir/../config.yaml --start 2019-02-11T00:00:00 --end 2019-02-18T00:00:00 $1
+  elastalert-test-rule --alert --config $basedir/../config.yaml --start 2019-02-11T00:00:00 --end 2019-02-18T00:00:00 $1
 else
   echo "Usage: test.sh <rule>"
 fi
